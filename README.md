c64b7f0eaedd154a9160bd6163aa816f91a7e985
atpboy444@hotmail.com <atpboy444@hotmail.com>
2017-03-16 18:03:05 +0800

    backup 001

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python for data analysis test project
--- usbabnames.py
+++ usbabnames.py
@@ -3,13 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-def add_prop(group):
-    """Integer division floors"""
-    births = group.births.astype(float)
-
-    group['prop'] = births / births.sum()
-    return group
-
 def hello():
     path = os.sys.path[0]
     path += r"\pydata-book-master\ch02\names"
@@ -26,8 +19,8 @@ def hello():
         frame['year'] = year
         pieces.append(frame)
     names = pd.concat(pieces, ignore_index=True)
-    print type(names)
-    print names[:10]
+    #print type(names)
+    #print names[:10]
 
     total_births = names.pivot_table(values='births', index='year', columns='sex', aggfunc=sum)
     #print "Total births by sex and year"
@@ -35,7 +28,16 @@ def hello():
     total_births.plot(kind='line', title='Total births by sex and year')
     #plt.show()
 
+    def add_prop(group):
+        """Integer division floors"""
+        births = group.births.astype(float)
+
+        group['prop'] = births / births.sum()
+        return group
+
     names = names.groupby(['year', 'sex']).apply(add_prop)
+    print "<names>:"
+    print names.head()
     #print type(names2)
     #print names2[:10]
     #names3 = names2.groupby(['year', 'sex']).prop.sum()
@@ -44,9 +46,17 @@ def hello():
     def get_top1000(group):
         return group.sort_values(by='births', ascending=False)[:1000]
     grouped = names.groupby(['year', 'sex'])
+    i = 0
+    for year, group in grouped:
+        print year
+        print group.head()
+        i = i + 1
+        if i > 2:
+            break
     top1000 = grouped.apply(get_top1000)
     print("top1000 names: %s" % type(top1000))
-    print top1000[:5]
+    print top1000
+
     name_births = top1000.pivot_table(values='births', index='year', columns='name', aggfunc=sum)
     subset = name_births[['John', 'Harry', 'Mary', 'Marilyn']]
     #print type(subset)
@@ -57,20 +67,77 @@ def hello():
     #print table[:5]
     table.plot(yticks=np.linspace(0, 1.2, 13), xticks=range(1880, 2020, 10), title='sum of prop by year and sex')
     #plt.show()
-    df = names[names.year == 2010][names.sex == 'M']
+    df = names[(names.year == 2010) & (names.sex == 'M')]
     #print df[:5]
     prop_cumsum = df.sort_values(by='prop', ascending=False).prop.cumsum()
     #print prop_cumsum[:5]
     #print prop_cumsum.searchsorted(0.5)
     def get_quantile_count(group, q=0.5):
         group = group.sort_values(by='prop', ascending=False)
-        prop_cumsum = group.prop.cumsum().searchsorted(q) + 1
+        index = group['prop'].cumsum().searchsorted(q)[0]
+        #print index
+        prop_cumsum = index + 1
+        #print type(prop_cumsum)
         return prop_cumsum
 
-    diversity = names.groupby(['year', 'sex']).apply(get_quantile_count)
+    diversity = names.groupby(['year', 'sex'])
+    #print type(diversity)
+    diversity = diversity.apply(get_quantile_count)
     #print type(diversity)
     diversity = diversity.unstack('sex')
-    print type(diversity)
-    print diversity
+    #print type(diversity)
+    #print diversity[:5]
     diversity.plot(title="Order of popular name at top 50%")
+    #plt.show()
+
+    get_last_letter = lambda x: x[-1]
+    last_letters = names.name.map(get_last_letter)
+    #print type(last_letters)
+    #last_letters.name = 'last_letter'
+    #print last_letters[]
+
+    table = names.pivot_table(values='births', index=last_letters, columns=['sex', 'year'], aggfunc=sum)
+    #print table.head()
+    subtable = table.reindex(columns=[1910, 1960, 2010], level='year').fillna(0)
+    print "<subtable>:"
+    print subtable.head()
+    print "sum:"
+    #subtable_sum = subtable.sum(axis=1, level='sex')
+    #print subtable_sum.head()
+    subtable_sum = subtable.sum(axis=0)
+    #print subtable_sum
+
+    #print subtable_sum.head()
+    #subtable_sum2 = subtable_sum.apply(lambda x: x.sum(), axis=1)
+    #print subtable_sum2
+    letter_prop = subtable / subtable_sum.astype(float)
+    print "prop:"
+    print letter_prop.head()
+    fig, axes = plt.subplots(2, 1,  figsize=(10, 8))
+    letter_prop['M'].plot(kind='bar', rot=0, ax=axes[0], title='Male')
+    letter_prop['F'].plot(kind='bar', rot=0, ax=axes[1], title='Female')
+    #plt.show()
+
+    letter_prop = table /table.sum(axis=0)
+    dny_ts = letter_prop.loc[['d', 'n', 'y'], 'M'].T
+    #print dny_ts
+    dny_ts.plot(kind='line', title="d/n/y by years")
+    #plt.show()
+
+    all_names = top1000.reset_index(drop=True).name.unique()
+    print "<all names>:"
+    print all_names
+    mask = np.array(['lesl' in x.lower() for x in all_names])
+    lesley_like = all_names[mask]
+    #print lesley_like
+    filtered = top1000[top1000.name.isin(lesley_like)].reset_index(drop=True)
+    print "<top1000 lesley>:"
+    print filtered.head()
+    #print filtered.groupby('name').apply(lambda x: x['births'].sum())
+    table = filtered.pivot_table(values='births', index='year', columns='sex', aggfunc='sum')
+    print table.head()
+    print table.sum(1)
+    table = table.div(table.sum(1), axis=0)
+    print table.tail()
+    table.plot(style = {'M': 'k-', 'F': 'k--'})
     plt.show()
